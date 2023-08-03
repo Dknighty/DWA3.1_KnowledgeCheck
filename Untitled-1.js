7c3bfa8ec669e4d2d6d5ccfdc6aeac9423e1b449
Aidan Poole <aidanjamie17@gmail.com>
2023-08-03 12:12:22 +0200

    .

---
+++ Untitled-1.js
@@ -0,0 +1,12 @@
+//@ts-check 
+
+/**
+ * a simple function to return the sum of two numbers
+ * @param {number} number1
+ * @param {number} number2 
+ * @returns {number}
+ */
+
+const addThese = (number1, number2) => {
+    return 1 + 2; 
+}
